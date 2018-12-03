24991e9506d197c9952f5f4af2e28160126900cf
Victor Fernandes <victor.m.fernandes@outlook.com>
2018-12-03 17:19:54 -0200

    teste

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ from flask import Flask, jsonify, request, abort
 
 import os
 
-from security import authenticate, identity
-from resources.user import UserRegister
-from resources.item import Item, ItemList
-from resources.store import Store, StoreList
+from classes.usuario import Usuario
 
 app = Flask(__name__)
 
@@ -25,14 +22,6 @@ def criar_usuario():
 
     resposta = jsonify({"task": usuario})
     return resposta, 201
-
-@app.route('/teste', methods=['POST'])
-def testar():
-    request_data = request.get_json()
-    resposta = {
-        "teste": request_data['teste']
-    }
-    return jsonify(resposta), 201
 
 if __name__ == '__main__':
     app.run(port=5000)
